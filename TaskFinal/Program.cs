459dd3f3ceb61c509a150e6bf16b40f9553db62f
Robert <IlyasovRV@gmai.comt>
2022-11-14 21:35:28 +0500

    Добавлено решение

---
+++ TaskFinal/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.Clear();
+
+int size = 5;
+string[] array = new string[size];
+int num = 3;
+Console.WriteLine($"Введите символы для заполнения массива размером {size}: ");
+FillArray(array);
+Console.Clear();
+PrintArray(array);
+Console.WriteLine();
+PrintArray(CreateSecondArray(array));
+
+void FillArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine()!;
+    }
+}
+
+void PrintArray(string[] array)
+{
+    Console.WriteLine();
+    Console.WriteLine("[" + String.Join(", ", array) + "]");
+}
+
+int SortedArray(string[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length;i++)
+    {
+        if (array[i].Length <= num)
+        {
+            count++;
+        }
+            
+    }
+    return count;
+}
+
+string[] CreateSecondArray(string[] array)
+{
+    string[] SecondArray = new string[SortedArray(array)];
+
+    for (int i = 0, j = 0; i < size; i++)
+    {
+          if (array[i].Length <= num)
+          {
+            SecondArray[j] = array[i];
+            j++;
+          }
+    }
+      return SecondArray;
+}
